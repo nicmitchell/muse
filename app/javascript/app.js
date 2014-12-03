d88b0ed44c1ed786937c5038cd9b6914839f4447
Nic Mitchell <nic@nicmitchell.com>
2014-12-03 16:28:55 -0700

    skeleton and echonest API

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,1 +1,23 @@
-app.js
+'use strict';
+
+var muse = angular.module('muse', [
+  'services',
+  'angular-echonest'
+]);
+
+muse.config(['EchonestProvider', function(EchonestProvider) {
+  EchonestProvider.setApiKey('CNQ7EJLGCHNW8QOIT');
+}]);
+
+
+Echonest.songs.search({
+  artist: 'radiohead',
+  title: 'karma police'
+}).then(function(songs) {
+  console.log(songs); // -> [{artist_id: "ARH6W4X1187B99274F", artist_name: "Radiohead", id: "SOHJOLH12A6310DFE5", title: "Karma Police"}, {...}]
+});
+
+
+
+
+
